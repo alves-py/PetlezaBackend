1a489ac17f7d8d6d10992b020d8a2c3003053509
Diogines Augusto <dioginisaugusto1@gmail.com>
2023-05-29 16:25:34 -0300

    Start

---
+++ src/main/java/com/example/petlezaBack/PetlezaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.petlezaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetlezaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetlezaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petlezaBack/PetlezaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petlezaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlezaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
